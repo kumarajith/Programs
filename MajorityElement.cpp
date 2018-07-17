37d13300dd9a4a39854efe110b7a68b79aee74d9
Ajith Kumar <kumarajith1996@gmail.com>
2018-07-17 22:16:14 +0530

    Completed Program

--- MajorityElement.cpp
+++ MajorityElement.cpp
@@ -2,12 +2,33 @@
 
 void printMajorityElement(int values[], int size)
 {
-
+    int i, candidate = values[0], count = 1;
+    for(i=1;i<size;i++)
+    {
+        if(values[i]==candidate)
+            count++;
+        else
+            count--;
+        if(count == 0)
+        {
+            candidate = values[i];
+            count = 1;
+        }
+    }
+    for(i=0;i<size;i++)
+    {
+        if(values[i] == candidate)
+            count++;
+    }
+    if(count>=size/2)
+        std::cout<<"Majority element is "<<candidate;
+    else
+        std::cout<<"No majority element found";
 }
 
 int main()
 {
-    int values = [1, 2, 3, 2, 2, 3, 2, 3, 3, 1, 1, 2];
-    printMajorityElement(values, 12);
+    int values[] = {1, 2, 3, 2, 2, 3, 2, 3, 3, 1, 1, 2, 2, 2};
+    printMajorityElement(values, 14);
     return 0;
 }
